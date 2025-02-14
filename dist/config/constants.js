f05afeb438c6e0667d1b4dd30695be6ae5c2636d
crypto-j09 <webdev0009@gmail.com>
2025-02-14 15:49:36 -0500

    first commit

---
+++ dist/config/constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.decimals = exports.tokenContractAddress = exports.arbRpcEndpoint = void 0;
+exports.arbRpcEndpoint = "https://sepolia-rollup.arbitrum.io/rpc";
+exports.tokenContractAddress = "0xEC10B8C166430D30E90DE7d070C9b5a0e8d25De9";
+exports.decimals = 18;
